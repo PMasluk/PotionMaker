06d228f19c90743b269d6a23e55512068a17514b
Paulina <paulina.masluk@o2.pl>
2023-07-30 15:27:17 +0200

    Remove collider

--- Assets/Scripts/PotEntering.cs
+++ Assets/Scripts/PotEntering.cs
@@ -6,9 +6,8 @@ public class PotEntering : MonoBehaviour
     private IndexAssigner assignIndex;
     private void Update()
     {
-        if ((transform.position.y > 0.5f && transform.position.y < 1) && (transform.position.x > -2.5f && transform.position.x < -1.5f))
+        if ((transform.position.y > -0.5f && transform.position.y < 1) && (transform.position.x > -2.7f && transform.position.x < -1.5f))
         {
-            //PotionListMaker.Instance.AddToList(assignIndex.GettingIndex);
             RecipesBase.Instance.CompareIngradientWithRecipe(assignIndex.GettingIndex);
             gameObject.SetActive(false);
         }
